e9d9cf493f59f0cb6bb92ac543bf078e756e7790
Pradeep Susarla <pradeep@pradeeps.org>
2015-04-26 21:32:48 +0100

    fb intergation 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#hoem'
 
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
