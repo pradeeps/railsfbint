c93216bb784ce78f2caf71e78ba0a83b1ae4f8a4
Pradeep Susarla <pradeep@pradeeps.org>
2015-04-26 20:05:23 +0100

    fb integration 1

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, "1477418045830693", "229aedb0870e50581b6f62411382e6a7"
+end
