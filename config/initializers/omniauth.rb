8559fd482733ef2f6716036675631cfd54253f29
Pradeep Susarla <pradeep@pradeeps.org>
2015-04-26 21:45:20 +0100

    session_controller

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,14 +4,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, "1477418045830693", "229aedb0870e50581b6f62411382e6a7"
 end
 
-class SessionsController < ApplicationController
-  def create
-    user = User.omniauth(env['omniauth.auth'])
-    session[:user_id] = user.id
-    redirect_to root_url
-  end
-
-  def destroy
-    session[:user_id] = nil
-  end
-end
